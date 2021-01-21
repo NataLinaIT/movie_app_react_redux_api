b615da9652ffa96270c83e928afdccdd6c2b9013
NataLina <kalipsoN80@gmail.com>
2021-01-21 14:22:57 +0200

    Updates

---
+++ precache-manifest.15db3ba60b2d6d73fe10b8155bb39c23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b744178693f700f5d7de3c245b2c3235",
+    "url": "/movie_app_react_redux_api/index.html"
+  },
+  {
+    "revision": "f70c32a565d558734680",
+    "url": "/movie_app_react_redux_api/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "b322b95a1cd648330a9f",
+    "url": "/movie_app_react_redux_api/static/css/main.1c891100.chunk.css"
+  },
+  {
+    "revision": "f70c32a565d558734680",
+    "url": "/movie_app_react_redux_api/static/js/2.b936e416.chunk.js"
+  },
+  {
+    "revision": "4c7326d1825e209363fe49c463938b53",
+    "url": "/movie_app_react_redux_api/static/js/2.b936e416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b322b95a1cd648330a9f",
+    "url": "/movie_app_react_redux_api/static/js/main.799c0a01.chunk.js"
+  },
+  {
+    "revision": "823afbfb47b6b9508012",
+    "url": "/movie_app_react_redux_api/static/js/runtime-main.1e27395d.js"
+  }
+]);
